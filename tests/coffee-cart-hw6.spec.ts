6f6e17f3fdebe92bda8d8c50cb66bc7788ebc600
alexad-aqa <alexadunka3545@gmail.com>
2026-09-23 19:43:51 +0300

    hw6-rework: coffee-cart tests using css selectors

--- tests/coffee-cart-hw6.spec.ts
+++ tests/coffee-cart-hw6.spec.ts
@@ -6,52 +6,52 @@ test.describe('coffee-cart-hw6', {tag: '@hw6'} , () => {
     });
 
 test('Item Removed from Tooltip List via Minus Icon ', async ({ page }) => {
-  await page.getByTestId('Cafe_Latte').click();
-  await expect(page.getByTestId('checkout')).toContainText('$16.00');
-  await expect(page.getByRole('link', { name: 'Cart page' })).toContainText('(1)');
-  await page.getByTestId('checkout').hover();
-  await page.getByRole('button', { name: 'Remove one Cafe Latte' }).click();
-  await expect(page.getByTestId('checkout')).toContainText('$0.00');
-  await expect(page.getByRole('link', { name: 'Cart page' })).toContainText('(0)');
+  await page.locator('[data-test="Cafe_Latte"]').click();
+  await expect(page.locator('[data-test="checkout"]')).toContainText('$16.00');
+  await expect(page.locator('[aria-label="Cart page"]')).toContainText('(1)');
+  await page.locator('[data-test="checkout"]').hover();
+  await page.locator('[aria-label="Remove one Cafe Latte"]').click();
+  await expect(page.locator('[data-test="checkout"]')).toContainText('$0.00');
+  await expect(page.locator('[aria-label="Cart page"]')).toContainText('(0)');
 });
 
 test('Discount Promo Dismissed', async ({ page }) => {
-  await page.getByTestId('Cappuccino').click();
-  await page.getByTestId('Americano').click();
-  await page.getByTestId('Espresso_Macchiato').click();
-  await page.getByRole('button', { name: 'Nah, I\'ll skip.' }).click();
-  await expect(page.getByText('It\'s your lucky day! Get an extra cup of Mocha for $4.espressochocolate')).toBeHidden();
-  await page.getByTestId('checkout').hover();
-  await expect(page.getByText('(Discounted) Mocha x 1+-')).toBeHidden();
+  await page.locator('[data-test="Cappuccino"]').click();
+  await page.locator('[data-test="Americano"]').click();
+  await page.locator('[data-test="Espresso_Macchiato"]').click();
+  await page.locator('.buttons button').nth(1).click();
+  await expect(page.locator('.promo')).toBeHidden();
+  await page.locator('[data-test="checkout"]').hover();
+  await expect(page.locator('.cart-preview .list-item')).toHaveCount(3)
 });
 
 test('Discounted Item Added to Cart', async ({ page }) => {
-  await page.getByTestId('Espresso').click();
-  await page.getByTestId('Espresso_Macchiato').click();
-  await page.getByTestId('Cappuccino').click();
-  await page.getByRole('button', { name: 'Yes, of course!' }).click();
-  await page.getByTestId('checkout').hover();
-  await expect(page.getByText('(Discounted) Mocha x 1+-')).toBeVisible();
-  await expect(page.getByTestId('checkout')).toContainText('Total: $45.00');
-  await expect(page.getByRole('link', { name: 'Cart page' })).toContainText('(4)');  
+  await page.locator('[data-test="Espresso"]').click();
+  await page.locator('[data-test="Espresso_Macchiato"]').click();
+  await page.locator('[data-test="Cappuccino"]').click();
+  await page.locator('.buttons button').nth(0).click();
+  await page.locator('[data-test="checkout"]').hover();
+  await expect(page.locator('.cart-preview .list-item').nth(0)).toContainText('(Discounted) Mocha');
+  await expect(page.locator('.cart-preview .list-item')).toHaveCount(4)
+  await expect(page.locator('[data-test="checkout"]')).toContainText('Total: $45.00');
+  await expect(page.locator('[aria-label="Cart page"]')).toContainText('(4)');  
 });
 
 test('Coffee Purchased Success Test', async ({ page }) => {
-  await page.getByTestId('Cafe_Latte').click();
-  await page.getByTestId('checkout').click();
-  await page.getByRole('textbox', { name: 'Name' }).fill('User');
-  await page.getByRole('textbox', { name: 'Email' }).click();
-  await page.getByRole('textbox', { name: 'Email' }).fill('user@gmail.com');
-  await page.getByRole('button', { name: 'Submit' }).click();
-  await expect(page.getByRole('button', { name: 'Thanks for your purchase.' })).toBeVisible();
+  await page.locator('[data-test="Cafe_Latte"]').click();
+  await page.locator('[data-test="checkout"]').click();
+  await page.locator('#name').fill('User');
+  await page.locator('#email').fill('user@gmail.com');
+  await page.locator('#submit-payment').click();
+  await expect(page.locator('[class~="snackbar"]')).toBeVisible();
 });
 
-test('Increase Coffee Qty by 1 via + Icon', async ({ page }) => {
-  await page.getByTestId('Espresso').click();
-  await page.getByRole('link', { name: 'Cart page' }).click();
-  await page.getByRole('button', { name: 'Add one Espresso' }).click();
+test('Increase Coffee Qty by 1 via + Icon on Cart Page', async ({ page }) => {
+  await page.locator('[data-test="Espresso"]').click();
+  await page.locator('[aria-label="Cart page"]').click();
+  await page.locator('[aria-label="Add one Espresso"]').nth(1).click();
   await expect(page.locator('#app')).toContainText('x 2');
-  await expect(page.getByRole('link', { name: 'Cart page' })).toContainText('(2)');
+  await expect(page.locator('[aria-label="Cart page"]')).toContainText('(2)');
   await expect(page.locator('#app')).toContainText('$20.00');
 });
 
